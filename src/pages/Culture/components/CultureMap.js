597243dd14a94d11092bfb8d98cae545ed5fe7d8
GitHub-SJL <dltkdwns2475@naver.com>
2023-03-12 10:08:20 +0900

    feat: 마커 마우스 이벤트 추가

--- src/pages/Culture/components/CultureMap.js
+++ src/pages/Culture/components/CultureMap.js
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk';
 
 import axios from 'axios';
 
 const CultureMap = () => {
   const [mapData, setMapData] = useState([]);
+  // 인포윈도우 Open 여부를 저장하는 state 입니다.
 
   useEffect(() => {
     axios
-      .get(' http://localhost:8000/DATA')
+      .get('http://localhost:8000/DATA')
       .then(Response => {
         setMapData(Response.data);
       })
@@ -18,32 +19,44 @@ const CultureMap = () => {
       });
   }, []);
 
-  return (
-    <Map
-      center={{ lat: 37.59508000957669, lng: 127.04930180082712 }}
-      style={{ width: '75%', height: '100%' }}
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: '#000' }}>Hello World!</div>
+  const EventMarkerContainer = ({ position, content }) => {
+    const map = useMap();
+    const [isVisible, setIsVisible] = useState(false);
+    console.log(mapData);
+    return (
+      <MapMarker
+        position={position} // 마커를 표시할 위치
+        // @ts-ignore
+        onClick={marker => map.panTo(marker.getPosition())}
+        onMouseOver={() => setIsVisible(true)}
+        onMouseOut={() => setIsVisible(false)}
+      >
+        {isVisible && content}
       </MapMarker>
-      <MapMarker // 마커를 생성합니다
-        position={{
-          // 마커가 표시될 위치입니다
-          lat: 37.59508000957669,
-          lng: 127.04930180082712,
-        }}
-      />
-      {mapData.map((data, idx) => {
-        return (
-          <MapMarker
-            key={idx}
-            position={{
-              lat: data.x_coord,
-              lng: data.y_coord,
-            }}
-          />
-        );
-      })}
+    );
+  };
+
+  return (
+    <Map // 지도를 표시할 Container
+      center={{
+        // 지도의 중심좌표
+        lat: 37.5696296,
+        lng: 126.9721884,
+      }}
+      style={{
+        // 지도의 크기
+        width: '100%',
+        height: '450px',
+      }}
+      level={8} // 지도의 확대 레벨
+    >
+      {mapData.map(value => (
+        <EventMarkerContainer
+          key={`EventMarkerContainer-${value.x_coord}-${value.y_coord}`}
+          position={{ lat: value.x_coord, lng: value.y_coord }}
+          content={<div style={{ color: '#000' }}>{value.fac_name}</div>}
+        />
+      ))}
     </Map>
   );
 };
