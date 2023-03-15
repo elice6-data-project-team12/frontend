2abaa1b7bf1d29ff4b41eadb46919cd70673a89e
GitHub-SJL <dltkdwns2475@naver.com>
2023-03-16 01:15:55 +0900

    style: icon 폴더명 수정

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,10 @@
-import ico_picker01_on from '../images/icons/ico_picker01_on.png';
-import ico_picker02_on from '../images/icons/ico_picker02_on.png';
-import ico_picker03_on from '../images/icons/ico_picker03_on.png';
-import ico_picker04_on from '../images/icons/ico_picker04_on.png';
-import ico_picker05_on from '../images/icons/ico_picker05_on.png';
-import ico_picker06_on from '../images/icons/ico_picker06_on.png';
-import ico_picker07_on from '../images/icons/ico_picker07_on.png';
+import ico_picker01_on from '../images/cultureIcons/ico_picker01_on.png';
+import ico_picker02_on from '../images/cultureIcons/ico_picker02_on.png';
+import ico_picker03_on from '../images/cultureIcons/ico_picker03_on.png';
+import ico_picker04_on from '../images/cultureIcons/ico_picker04_on.png';
+import ico_picker05_on from '../images/cultureIcons/ico_picker05_on.png';
+import ico_picker06_on from '../images/cultureIcons/ico_picker06_on.png';
+import ico_picker07_on from '../images/cultureIcons/ico_picker07_on.png';
 
 export {
   ico_picker01_on,
