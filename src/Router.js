679032bdeaacf7e8961c167bbb48f4d71d2c7654
heyhye94 <heyhye94@gmail.com>
2023-03-17 18:47:32 +0900

    feat: 회원가입, 로그인

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,9 @@ import Nav from './layout/Nav.js';
 import Footer from './layout/Footer.js';
 import Landing from './pages/Landing/LandingPage';
 import Culture from './pages/Culture/CulturePage.js';
+import SignupPage from './pages/signup/SignupPage';
+import LoginPage from './pages/Login/LoginPage'
+import ItemPage from './pages/Board/ItemPage'
 
 function Router() {
   return (
@@ -14,6 +17,9 @@ function Router() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/culture" element={<Culture />} />
+          <Route path="/user/signup" element={<SignupPage />} />
+          <Route path="/user/login" element={<LoginPage />} />
+          <Route path="/item" element={<ItemPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/pages/signup/signupPage.js
+++ src/pages/signup/signupPage.js
@@ -1,1 +1,95 @@
-// 회원가입페이지
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import SignupForm from './components/SignupForm'
+
+
+function SignupPage() {
+
+    const navigate = useNavigate();
+
+    return(
+    <Container>
+      <MainTitle>
+        <span>회원가입</span>
+      </MainTitle>
+      <UserInputBox>
+      <SignupForm />
+        <button
+          onClick={() => {
+            navigate('/user/login');
+          }}
+        >
+          로그인하러 가기
+        </button>
+        <Link className="link-to-home" to="/">
+          {'>'} 홈으로 이동
+        </Link>
+      </UserInputBox>
+    </Container>
+    )
+}
+
+const Container = styled.div`
+  margin: 15% 0 5% 0;
+  width: 100%;
+  height: max-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 4px groove red;
+`;
+
+const MainTitle = styled.div`
+  width: 35%;
+  font-weight: 600;
+  font-size: 25px;
+  display: flex;
+  justify-content: center;
+  border: 4px groove red;
+`;
+
+const UserInputBox = styled.div`
+  width: 50%;
+  height: 75vh;
+  background-color: rgba(217, 217, 217, 1);
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  .error-msg {
+    color: red;
+    font-size: 12px;
+    padding-top: 0px;
+    border: 4px groove red;
+  }
+  form {
+    height: 60%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border: 4px groove red;
+     div {
+      width: 80%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border: 4px groove red;
+       .form-field {
+        width: 70%;
+         input {
+          width: 100%;
+          background-color: inherit;
+        }
+      }
+    }
+  }
+`;
+
+
+export default SignupPage;
