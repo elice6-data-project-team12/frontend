22d34a1a9bdcea168de854f5018c9f799dc79b80
GitHub-SJL <dltkdwns2475@naver.com>
2023-03-19 16:49:44 +0900

    refactor: 불필요한 파일 삭제

--- src/pages/Culture/components/table/Table.js
+++ src/pages/Culture/components/table/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useTable, usePagination } from 'react-table';
 const Table = ({
   columns,
