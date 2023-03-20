19f4aecf86b00da96e39f3a389b36fb0c235cead
GitHub-SJL <dltkdwns2475@naver.com>
2023-03-20 22:35:30 +0900

    feat: 랜딩페이지 지도 마커, 데이터 바인딩

--- src/pages/Landing/components/charts/MapChart.js
+++ src/pages/Landing/components/charts/MapChart.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import styled from 'styled-components';
 import seoulMap from 'pages/Landing/Data/map/seoul.json';
 
 // components
@@ -26,8 +26,54 @@ const MapChart = () => {
 
   const [modal, setModal] = useState('none');
   // 모달종류 : pw, chk, deny, pw_delete, pw_update, none
-
-  return <Map currentState={currentState} />;
+  console.log(currentState);
+  return (
+    <>
+      <Map currentState={currentState} />;
+      <Legend>
+        <div className="ranking">
+          <dl>
+            <dt>1인 가구수</dt>
+            <dd></dd>
+            <div className="range">
+              <div>0</div>
+              <div>700000</div>
+            </div>
+          </dl>
+        </div>
+      </Legend>
+    </>
+  );
 };
 
+const Legend = styled.div`
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  padding: 16px;
+  background: rgba(255, 255, 255, 0.6);
+  border: 1px solid gray;
+  border-radius: 4px;
+  z-index: 990;
+
+  .ranking {
+    dl {
+      dd {
+        margin-left: 8px;
+        width: 160px;
+        height: 16px;
+        background: linear-gradient(
+          to right,
+          rgba(245, 80, 115, 0.1) 0%,
+          rgba(245, 80, 115, 1) 100%
+        );
+      }
+
+      .ranking {
+        display: flex;
+        justify-content: center;
+      }
+    }
+  }
+`;
 export default MapChart;
