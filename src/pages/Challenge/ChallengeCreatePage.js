c121ef41e9f2dd2c4349aa02195f5d99e4f210ce
go1jamuae <go1jamuae@gmail.com>
2023-03-23 19:02:36 +0900

    feat: 챌린지수정삭제,CSS수정

--- src/pages/Challenge/ChallengeCreatePage.js
+++ src/pages/Challenge/ChallengeCreatePage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import ChallengeForm from './components/ChallengeItems/ChallengeForm';
+import LoginPage from 'pages/Login/LoginPage';
 
 const ChallengeCreatePage = () => {
+  const isLoggedIn = !!localStorage.getItem('userToken');
+
+  if (!isLoggedIn) {
+    return <LoginPage />;
+  }
+
   return (
     <div>
       <ChallengeForm actionType="create" />
